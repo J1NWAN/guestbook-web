a7873a78f780c41316554f7c2b5ecc413976e060
J1NWAN <61576254+J1NWAN@users.noreply.github.com>
2021-12-11 23:37:24 +0900

    Initial commit

---
+++ src/main/java/guestbook/guestbookweb/GuestbookWebApplication.java
@@ -0,0 +1,13 @@
+package guestbook.guestbookweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestbookWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestbookWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guestbook/guestbookweb/GuestbookWebApplicationTests.java
@@ -0,0 +1,13 @@
+package guestbook.guestbookweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestbookWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
